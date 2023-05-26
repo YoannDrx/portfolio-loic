8448375cb536dbe26449b4da3d14d49e9489d64a
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-05-26 12:38:49 +0200

    copyright

--- components/CopyRight.jsx
+++ components/CopyRight.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 
 const CopyRight = () => {
-  return (
-    <div className="copyright">
-      <p>
-        &copy; {new Date().getFullYear()} Tokyo <br /> Created by
-        <a
-          href="https://www.yodev.fr/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Yodev
-        </a>
-      </p>
-    </div>
-  );
+    return (
+        <div className="copyright">
+                Created by
+            <span>
+                <a href="https://www.yodev.fr/" target="_blank" rel="noreferrer">
+                &copy; Yodev 
+                </a>
+            </span>
+            <span> {new Date().getFullYear()}</span>
+        </div>
+    );
 };
 
 export default CopyRight;
