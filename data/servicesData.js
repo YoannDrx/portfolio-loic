2bfb6502f02f67066583fdfad8d7aa50025c2fba
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-05-31 00:38:49 +0200

    SERVICE done

--- data/servicesData.js
+++ data/servicesData.js
@@ -3,19 +3,19 @@ module.exports = [
         id: 1,
         no: "01",
         title: "Composition",
-        text: `The creation of original, captivating melodies is Loïc's speciality. Discover more...`,
+        text: `Creating original, captivating melodies is my speciality. Discover more...`,
         largeImg: "/img/services/composition.jpg",
         largeTitle: "Composition: Crafting Unique and Captivating Melodies",
-        poster: "Anna Deploy - Blog writer",
+        poster: "Myself",
         date: "22 May 2023",
         descriptions: (
             <>
                 <p>
-                    Loïc Ghanem, a professional composer, has a deep passion for creating unique and captivating melodies. His expertise spans various genres, ensuring a wide range of possibilities
+                    As a professional composer, I have a deep passion for creating unique and captivating melodies. My expertise spans various genres, ensuring a wide range of possibilities
                     for your project.
                 </p>
                 <p>
-                    Loïc approaches each project with a fresh perspective, dedicating time and effort to understand your needs and preferences. This attention to detail allows him to create
+                    I approach each project with a fresh perspective, dedicating time and effort to understand your needs and preferences. This attention to detail allows me to create
                     compositions that are truly bespoke and aligned with your creative vision.
                 </p>
             </>
@@ -24,60 +24,59 @@ module.exports = [
     {
         id: 2,
         no: "02",
-        title: "Sound Design",
-        text: `Immerse your audience in a unique auditory experience with Loïc's sound design services. Discover more...`,
-        largeImg: "/img/services/sounddesign.jpg",
-        largeTitle: "Sound Design: Creating Immersive Auditory Experiences",
-        poster: "Anna Deploy - Blog writer",
+        title: "Music Production",
+        text: `From concept to final mix, I offer comprehensive music production services. Discover more...`,
+        largeImg: "/img/services/musicproduction.jpg",
+        largeTitle: "Music Production: From Concept to Final Mix",
+        poster: "Myself",
         date: "22 May 2023",
         descriptions: (
             <>
                 <p>
-                    With sound design, the smallest details can have a huge impact on the experience of your audience. Loïc is well-versed in the art of manipulating audio to create immersive
-                    environments and enhance narrative storytelling.
+                    My music production services encompass every stage of the process. With a deep understanding of music theory, a keen ear for detail, and proficiency in the latest
+                    production software, I am equipped to bring your musical ideas to fruition.
                 </p>
-                <p>
-                    From subtle ambient noise to dynamic sound effects, each element is carefully considered and meticulously crafted. Let Loïc&apos;s sound design services bring your project to life.
-                </p>
+                <p>Whether you need help with arranging, recording, mixing, or mastering, I provide a comprehensive solution tailored to your project&apos;s needs.</p>
             </>
         ),
     },
     {
         id: 3,
         no: "03",
-        title: "Music Production",
-        text: `From concept to final mix, Loïc offers comprehensive music production services. Discover more...`,
-        largeImg: "/img/services/musicproduction.jpg",
-        largeTitle: "Music Production: From Concept to Final Mix",
-        poster: "Anna Deploy - Blog writer",
+        title: "Arranging",
+        text: `Enhance your compositions with my expertise in arranging. Discover more...`,
+        largeImg: "/img/services/arranging.jpg",
+        largeTitle: "Arranging: Enhancing Your Compositions",
+        poster: "Myself",
         date: "22 May 2023",
         descriptions: (
             <>
                 <p>
-                    Loïc&apos;s music production services encompass every stage of the process. With a deep understanding of music theory, a keen ear for detail, and proficiency in the latest
-                    production software, he is equipped to bring your musical ideas to fruition.
+                    My arranging skills will add depth and complexity to your music. With my extensive knowledge of orchestration and harmony, I will craft arrangements that bring out the best
+                    in your compositions.
                 </p>
-                <p>Whether you need help with arranging, recording, mixing, or mastering, Loïc provides a comprehensive solution tailored to your project&apos;s needs.</p>
+                <p>Whether you have a fully formed composition or a simple melody,I can transform it into a rich, fully realized piece of music.</p>
             </>
         ),
     },
-    // Add more service data as needed
     {
         id: 4,
         no: "04",
-        title: "Arranging",
-        text: `Enhance your compositions with Loïc's expertise in arranging. Discover more...`,
-        largeImg: "/img/services/arranging.jpg",
-        largeTitle: "Arranging: Enhancing Your Compositions",
-        poster: "Anna Deploy - Blog writer",
+        title: "Vocal Producer",
+        text: `Immerse your audience in a unique auditory experience with my sound design services. Discover more...`,
+        largeImg: "/img/services/sounddesign.jpg",
+        largeTitle: "Sound Design: Creating Immersive Auditory Experiences",
+        poster: "Myself",
         date: "22 May 2023",
         descriptions: (
             <>
                 <p>
-                    Loïc's arranging skills will add depth and complexity to your music. With his extensive knowledge of orchestration and harmony, he will craft arrangements that bring out the best
-                    in your compositions.
+                    With sound design, the smallest details can have a huge impact on the experience of your audience. I am well-versed in the art of manipulating audio to create immersive
+                    environments and enhance narrative storytelling.
                 </p>
-                <p>Whether you have a fully formed composition or a simple melody, Loïc can transform it into a rich, fully realized piece of music.</p>
+                <p>
+                    From subtle ambient noise to dynamic sound effects, each element is carefully considered and meticulously crafted. Let my sound design services bring your project to life.
+                </p>
             </>
         ),
     },
@@ -85,18 +84,18 @@ module.exports = [
         id: 5,
         no: "05",
         title: "Mixing",
-        text: `Loïc's mixing services ensure your music sounds balanced and polished. Discover more...`,
+        text: `My mixing services ensure your music sounds balanced and polished. Discover more...`,
         largeImg: "/img/services/mixing.jpg",
         largeTitle: "Mixing: Polishing Your Music to Perfection",
-        poster: "Anna Deploy - Blog writer",
+        poster: "Myself",
         date: "22 May 2023",
         descriptions: (
             <>
                 <p>
-                    Mixing is a critical step in the music production process, and one that Loïc excels in. He will balance the elements of your track, ensuring that no instrument or voice is too loud
+                    Mixing is a critical step in the music production process, and one that I excel in. I will balance the elements of your track, ensuring that no instrument or voice is too loud
                     or too soft.
                 </p>
-                <p>His mixing techniques will also add depth and dimension to your music, creating a soundscape that is rich and engaging.</p>
+                <p>My mixing techniques will also add depth and dimension to your music, creating a soundscape that is rich and engaging.</p>
             </>
         ),
     },
@@ -104,18 +103,18 @@ module.exports = [
         id: 6,
         no: "06",
         title: "Mastering",
-        text: `Give your music the final polish it needs with Loïc's mastering services. Discover more...`,
+        text: `Give your music the final polish it needs with my mastering services. Discover more...`,
         largeImg: "/img/services/mastering.jpg",
         largeTitle: "Mastering: The Final Touch for Your Music",
-        poster: "Anna Deploy - Blog writer",
+        poster: "Myself",
         date: "22 May 2023",
         descriptions: (
             <>
                 <p>
-                    Mastering is the final step in the music production process, and it&apos;s one that Loïc takes very seriously. He will ensure your tracks are ready for distribution, optimizing
+                    Mastering is the final step in the music production process, and it&apos;s one that I take very seriously. I will ensure your tracks are ready for distribution, optimizing
                     them for all playback systems.
                 </p>
-                <p>Loïc&apos;s mastering services will give your music the final polish it needs, ensuring it sounds professional and stands up against other commercial releases.</p>
+                <p>My mastering services will give your music the final polish it needs, ensuring it sounds professional and stands up against other commercial releases.</p>
             </>
         ),
     },
