b2f501e78ef33c65e76329a47c9dadcf3a85b310
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-06-09 16:22:29 +0200

    fix path

--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,29 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-        <meta property="og:image" content="/img/og-loic.png" />
-        <meta property="og:title" content="Loïc Ghanem Music - Composer & Producer" />
-        <meta property="og:description" content="Découvrez la musique de Loïc Ghanem, compositeur et producteur." />
-        <meta name="description" content="Loïc Ghanem Music - Composer & Producer" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="robots" content="noindex, follow" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                    rel="stylesheet"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                    rel="stylesheet"
+                />
+                <meta property="og:image" content="https://www.loic-ghanem.com/img/og-loic.png" />
+                <meta property="og:title" content="Loïc Ghanem Music - Composer & Producer" />
+                <meta property="og:description" content="Découvrez la musique de Loïc Ghanem, compositeur et producteur." />
+                <meta name="description" content="Loïc Ghanem Music - Composer & Producer" />
+                <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                <meta name="robots" content="noindex, follow" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
