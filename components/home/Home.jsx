cb84846ff7fbc5e5c5d1ea0ec666d26a691e6922
YoannDrx <yoann.andrieux@gmail.com>
2024-02-29 21:10:55 +0100

    fix deploy

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -22,28 +22,6 @@ const Home = () => {
             <h4 className="typer">
               <Typed strings={["music composer", "music producer"]} loop typeSpeed={80} />
             </h4>
-            {/* 
-            <p className="job">
-              Composer based in Paris. I bring your projects to life with sound, specializing in composing music for films, video
-              games, and commercials. Elevate your narratives with my unique soundscapes.
-            </p>
-            <p className="job">
-              Nominated in 2023 for Best Rock and Hip-Hop Songs at the{" "}
-              <a href={"https://pmamusic.com/events/mark-awards/"} target="_blank" rel="noopener noreferrer">
-                Los Angeles Mark Awards
-              </a>
-              , winning in the Rock category ! Also nominated for the Rock category at the{" "}
-              <a href={"https://www.productionmusicawards.com/nominations-2023/"} target="_blank" rel="noopener noreferrer">
-                London Music Production Awards
-              </a>
-              .
-            </p>
-            <p style={{ marginBottom: "20px" }}>
-              Check out my last video{" "}
-              <a href="https://www.youtube.com/watch?v=sG9J5pcNco4" className="last-video">
-                &quot;Dark Industry&quot;
-              </a>
-            </p> */}
             <p className="job">
               Composer based in Paris. I bring your projects to life with sound, specializing in composing music for films, video
               games, and commercials. Elevate your narratives with my unique soundscapes.
@@ -75,7 +53,7 @@ const Home = () => {
                 className="hover-green"
                 target="_blank"
                 rel="noopener noreferrer">
-                "Dark Industry"
+                &quot;Dark Industry&quot;
               </a>
             </p>
             {/* END JOB */}
